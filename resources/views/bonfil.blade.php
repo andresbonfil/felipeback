4ea89292aec74fb36abde8a8ab8c09a82217fb10
Mr. Anderson Bonfil <andresbonfil@gmail.com>
2021-08-19 14:06:30 -0500

    linksonindex2

--- resources/views/bonfil.blade.php
+++ resources/views/bonfil.blade.php
@@ -3,6 +3,6 @@
 @section('contenido')
 <img src="{{asset('images/multitask.png')}}" alt="multitasking">
 <a href="{{route('emailRecovery?=')}}"></a><h2>emailecovery</h2>
-<a href="{{api/usuario')}}"></a><h2>usuario</h2>
+<a href="api/usuario"></a><h2>usuario</h2>
 <h2>We are working on it... working hard and fast!</h2>
 @endsection

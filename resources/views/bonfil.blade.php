608f9306b264f63cd4938bc81a8e86631e3e1879
Mr. Anderson Bonfil <andresbonfil@gmail.com>
2021-08-19 16:20:44 -0500

    linksonindex5

--- resources/views/bonfil.blade.php
+++ resources/views/bonfil.blade.php
@@ -2,7 +2,7 @@
 @section('title','Inicio paps')
 @section('contenido')
 <img src="{{asset('images/multitask.png')}}" alt="multitasking">
-<a href="emailRecovery?=testing@gmail.com"></a><h2>emailecovery</h2>
-<a href="api/usuario"></a><h2>usuario</h2>
+<a href="emailRecovery?=testing@gmail.com">emailecovery</a><h2></h2>
+<a href="api/usuario">usuario</a><h2></h2>
 <h2>We are working on it... working hard and fast!</h2>
 @endsection
